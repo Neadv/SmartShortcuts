0db106cc221274b5127e8bfa9cdb5c4105e23638
Vlad Krinitskiy <38957031+Neadekvashka@users.noreply.github.com>
2021-02-03 16:35:20 +0200

    Add main view model

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartShortcuts.ViewModel
+{
+    class MainViewModel
+    {
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
